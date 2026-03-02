4ff5c600f553d8d418a69b2e9d69ffe890f7cb20
sayon-work <sayon.work@gmail.com>
2026-03-02 16:57:49 +1100

    Get capital with response in JSON format.

--- src/main/java/ai/bharat/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/ai/bharat/springaiintro/services/OpenAIServiceImpl.java
@@ -3,10 +3,15 @@ package ai.bharat.springaiintro.services;
 import ai.bharat.springaiintro.model.Answer;
 import ai.bharat.springaiintro.model.GetCapitalRequest;
 import ai.bharat.springaiintro.model.Question;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -26,6 +31,9 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     @Value("classpath:templates/get-capital-with-info.st")
     private Resource getGetCapitalPromptWithInfo;
+
+    //@Autowired TODO figure why ObjectMapper autowiring failing
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public String getAnswer(String question) {
@@ -54,7 +62,20 @@ public class OpenAIServiceImpl implements OpenAIService {
 
         ChatResponse response = chatModel.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getText());
+        System.out.println(response.getResult().getOutput().getText());
+        String responseString;
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
+        //return new Answer(response.getResult().getOutput().getText());
     }
 
     @Override
